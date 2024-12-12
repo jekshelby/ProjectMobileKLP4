fc971e63e7e619f5b2bdce225823cc2050d9e413
salsa <salsa.biljannaty@gmail.com>
2024-12-12 11:16:53 +0700

    tambah komentar

--- calculator/lib/button.dart
+++ calculator/lib/button.dart
@@ -1,39 +1,41 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'; // Import library Flutter
 
 class MyButton extends StatefulWidget {
-  final Color color;
-  final Color textColor;
-  final String buttonText;
-  final VoidCallback? buttonTapped;
+  // Properti yang akan digunakan untuk tombol
+  final Color color; // Warna latar tombol
+  final Color textColor; // Warna teks tombol
+  final String buttonText; // Teks yang ditampilkan pada tombol
+  final VoidCallback? buttonTapped; // Fungsi yang akan dipanggil saat tombol ditekan
 
+  // Konstruktor untuk inisialisasi properti tombol
   const MyButton({
     Key? key,
-    this.color = Colors.blue,
-    this.textColor = Colors.white,
-    this.buttonText = "Button",
-    this.buttonTapped,
+    this.color = Colors.blue, // Warna default tombol biru
+    this.textColor = Colors.white, // Warna teks default putih
+    this.buttonText = "Button", // Teks default tombol
+    this.buttonTapped, // Fungsi callback untuk aksi tombol
   }) : super(key: key);
 
   @override
-  _MyButtonState createState() => _MyButtonState();
+  _MyButtonState createState() => _MyButtonState(); // Membuat state dari tombol
 }
 
 class _MyButtonState extends State<MyButton> {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: widget.buttonTapped,
+      onTap: widget.buttonTapped, // Memanggil fungsi callback saat tombol ditekan
       child: Padding(
-        padding: const EdgeInsets.all(8.0), // Tambahkan padding
+        padding: const EdgeInsets.all(8.0), // Memberikan padding di sekitar tombol
         child: Container(
           decoration: BoxDecoration(
-            color: widget.color,
-            borderRadius: BorderRadius.circular(20),
+            color: widget.color, // Mengatur warna latar tombol
+            borderRadius: BorderRadius.circular(20), // Membuat sudut tombol melengkung
           ),
           child: Center(
             child: Text(
-              widget.buttonText,
-              style: TextStyle(color: widget.textColor),
+              widget.buttonText, // Menampilkan teks tombol
+              style: TextStyle(color: widget.textColor), // Mengatur warna teks tombol
             ),
           ),
         ),
