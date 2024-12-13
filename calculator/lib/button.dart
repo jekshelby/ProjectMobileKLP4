4dd6851d9898e08b6a2c797c96e43ce42ac9a8d2
Muhammad Dzaky Ahnaf <ahnafdzaky9@gmail.com>
2024-12-13 16:33:24 +0700

    ui fixes

--- calculator/lib/button.dart
+++ calculator/lib/button.dart
@@ -1,19 +1,18 @@
-import 'package:flutter/material.dart'; // Import library Flutter
+import 'package:flutter/material.dart';
 
 class MyButton extends StatefulWidget {
-  // Properti yang akan digunakan untuk tombol
   final Color color; // Warna latar tombol
   final Color textColor; // Warna teks tombol
   final String buttonText; // Teks yang ditampilkan pada tombol
-  final VoidCallback?
-      buttonTapped; // Fungsi yang akan dipanggil saat tombol ditekan
+  final double fontSize; // Ukuran font dari teks tombol
+  final VoidCallback? buttonTapped; // Fungsi callback untuk aksi tombol
 
-  // Konstruktor untuk inisialisasi properti tombol
   const MyButton({
     Key? key,
     this.color = Colors.blue, // Warna default tombol biru
     this.textColor = Colors.white, // Warna teks default putih
     this.buttonText = "Button", // Teks default tombol
+    this.fontSize = 20.0, // Ukuran font default 20
     this.buttonTapped, // Fungsi callback untuk aksi tombol
   }) : super(key: key);
 
@@ -25,22 +24,21 @@ class _MyButtonState extends State<MyButton> {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap:
-          widget.buttonTapped, // Memanggil fungsi callback saat tombol ditekan
+      onTap: widget.buttonTapped, // Memanggil fungsi callback saat tombol ditekan
       child: Padding(
-        padding:
-            const EdgeInsets.all(8.0), // Memberikan padding di sekitar tombol
+        padding: const EdgeInsets.all(8.0), // Memberikan padding di sekitar tombol
         child: Container(
           decoration: BoxDecoration(
             color: widget.color, // Mengatur warna latar tombol
-            borderRadius:
-                BorderRadius.circular(20), // Membuat sudut tombol melengkung
+            borderRadius: BorderRadius.circular(20), // Membuat sudut tombol melengkung
           ),
           child: Center(
             child: Text(
               widget.buttonText, // Menampilkan teks tombol
               style: TextStyle(
-                  color: widget.textColor), // Mengatur warna teks tombol
+                color: widget.textColor, // Mengatur warna teks tombol
+                fontSize: widget.fontSize, // Mengatur ukuran teks
+              ),
             ),
           ),
         ),
